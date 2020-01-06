f26f4bc88812b8f55cca3bfc4e36a9b19b73923c
Ingi Helgason <helgason.ingi@gmail.com>
2020-01-05 20:48:10 -0800

    Add Ports class

---
+++ src/main/java/com/ironpanthers/frc2020/Ports.java
@@ -0,0 +1,16 @@
+package com.ironpanthers.frc2020;
+
+/**
+ * Ports contains all IDs on the robot. While similar to constants in theory, it
+ * should ONLY contain the hardware/firmware level IDs for electronic components
+ * of the robot (CAN bus ids, PCM ids, etc.).
+ */
+public final class Ports {
+
+    private Ports() {
+        /* disallow construction of this class */
+        throw new UnsupportedOperationException("don't try to construct an instance of Ports");
+    }
+
+    // public static final int kMyPort = 1;
+}
