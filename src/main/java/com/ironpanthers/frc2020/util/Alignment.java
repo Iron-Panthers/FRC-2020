b9fb54bdd71b67ddb64e1abe80870e6c23664d07
Fara <farayan365@gmail.com>
2020-01-06 14:18:55 -0800

    Add distance calculation method

---
+++ src/main/java/com/ironpanthers/frc2020/util/Alignment.java
@@ -0,0 +1,18 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package com.ironpanthers.frc2020.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Alignment{
+  public float calculateDistance(){
+    float d = (Constants.h2-Constants.h1) / tan(Constants.a1+ty); //ty: vertical offset angle in degrees
+    return d; 
+  }
+  
+}
