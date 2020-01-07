8352ed70978bc80f24c984c820184992b54fec3a
Chloe <chloeyan365@gmail.com>
2020-01-06 16:41:18 -0800

    Add table and entries to TurnToTarget command

--- src/main/java/com/ironpanthers/frc2020/commands/TurnToTarget.java
+++ src/main/java/com/ironpanthers/frc2020/commands/TurnToTarget.java
@@ -8,13 +8,32 @@
 package com.ironpanthers.frc2020.commands;
 
 import com.ironpanthers.frc2020.Constants;
+
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 public class TurnToTarget extends CommandBase {
-  /**
-   * Creates a new TurnToTarget.
-   */
+
+  public static NetworkTable table;
+  public static NetworkTableEntry tx;
+  public static NetworkTableEntry ty;
+  public static NetworkTableEntry ta;
+  public static NetworkTableEntry tv;
+  public static float x;
+  public static float y;
+  public static float v;
+
   public TurnToTarget() {
+    table = NetworkTableInstance.getDefault().getTable("limelight");
+    tx = table.getEntry("tx");
+    ty = table.getEntry("ty");
+    tv = table.getEntry("tv");
+    ta = table.getEntry("ta");
+    x = (float) tx.getDouble(0.0);
+    y = (float) ty.getDouble(0.0);
+    v = (float) tv.getDouble(0.0);
     // Use addRequirements() here to declare subsystem dependencies.
   }
 
@@ -26,23 +45,23 @@ public class TurnToTarget extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-
+    
     float leftSteeringAngle = 0.0f; // Amount of degrees to steer left
     float rightSteeringAngle = 0.0f; // Amount of degrees to steer right
-    float horizontalError = -tx; // Horizontal error from limelight to target
+    float horizontalError = -x; // Horizontal error from limelight to target
     float adjustedSteeringValue = 0.0f; // Calculated amount of degrees to steer
 
-    if (tv == 0.0f) {
+    if (v == 0.0) {
       adjustedSteeringValue = 0.3f;
     }
     else {
-      if (tx > 1.0) {
+      if (x > 0.0) {
         adjustedSteeringValue = Constants.Kp * horizontalError - Constants.MINIMUM_POWER;
-      } else if (tx < 1.0) {
+      } else if (x < 0.0) {
         adjustedSteeringValue = Constants.Kp * horizontalError + Constants.MINIMUM_POWER;
       }
     }
-
+    
     leftSteeringAngle += adjustedSteeringValue;
     rightSteeringAngle -= adjustedSteeringValue;
     
