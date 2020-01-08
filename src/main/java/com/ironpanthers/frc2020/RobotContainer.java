7e98a0995ff59360d13167bac947fc75fbd67fd2
Ilan Rosenbaum <ilan@al.net>
2020-01-07 17:04:57 -0800

    Deleted Useless Things

--- src/main/java/com/ironpanthers/frc2020/RobotContainer.java
+++ src/main/java/com/ironpanthers/frc2020/RobotContainer.java
@@ -9,8 +9,6 @@ package com.ironpanthers.frc2020;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
-import com.ironpanthers.frc2020.commands.ExampleCommand;
-import com.ironpanthers.frc2020.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 
 /**
@@ -22,10 +20,7 @@ import edu.wpi.first.wpilibj2.command.Command;
  */
 public class RobotContainer {
 	// The robot's subsystems and commands are defined here...
-	private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
-
-	private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
-
+	
 	/**
 	 * The container for the robot. Contains subsystems, OI devices, and commands.
 	 */
---
+++ src/main/java/com/ironpanthers/frc2020/util/VisionWrapper.java
@@ -0,0 +1,5 @@
+package com.ironpanthers.frc2020.util;
+
+public class VisionWrapper {
+    NetworkTableInstance.getDefault().getTable("limelight").getEntry("<variablename>").getDouble(0);
+}
