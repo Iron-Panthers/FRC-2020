e9b0f169941d2f8f744a5b2e6095843073e7016e
X1 Carbon Driver Station <5026craptop@gmail.com>
2020-01-28 20:15:16 -0800

    Update Shooter Velocity

--- src/main/java/com/ironpanthers/frc2020/Constants.java
+++ src/main/java/com/ironpanthers/frc2020/Constants.java
@@ -128,7 +128,7 @@ public final class Constants {
         public static final double SHOOTER_MAX_SAFE_VEL = 12000; // Native units
 		public static final double SHOOTER_RAMP_RATE = 1.; // Seconds to full power during PID control + Open Loop
 		public static final int SHOOTER_VELOCITY_THRESHOLD = 300; // Acceptable error in velocity before shooting
-		public static final int SHOOTER_TEST_VELOCITY = 5000;
+		public static final int SHOOTER_TEST_VELOCITY = 15000;
 	}
 	
 	public final class Arm {
