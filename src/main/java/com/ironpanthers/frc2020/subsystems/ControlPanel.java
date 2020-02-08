13403a7dd39d4b88f05e9142e29752ff7b09e183
Fara <farayan365@gmail.com>
2020-02-08 00:01:02 -0800

    Add position control method

--- src/main/java/com/ironpanthers/frc2020/subsystems/ControlPanel.java
+++ src/main/java/com/ironpanthers/frc2020/subsystems/ControlPanel.java
@@ -7,11 +7,14 @@
 
 package com.ironpanthers.frc2020.subsystems;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.ironpanthers.frc2020.Constants;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
+import edu.wpi.first.wpilibj.DriverStation;
 public class ControlPanel extends SubsystemBase {
   /**
    * Creates a new ControlPanel.
@@ -22,13 +25,40 @@ public class ControlPanel extends SubsystemBase {
     controlPanelMotor = new TalonFX(Constants.CONTROL_PANEL_MOTOR_PORT);
   }
 
-  public static void colorRotation(int rotations, int direction) { // Direction: -1 for left, 1 for right
+  public static void colorRotation(int rotations, int direction) { // Direction: -1 for counter-clockwise, 1 for clockwise
     controlPanelMotor.setSelectedSensorPosition(0);
     double distance = rotations * Constants.ENCODER_TICKS_PER_INCH * 12.5; // Each rotation is 12.5 inches apart
     if(controlPanelMotor.getSelectedSensorPosition() < distance){
       controlPanelMotor.set(ControlMode.PercentOutput, direction * Constants.CONTROL_PANEL_MOTOR_SPEED);
     } else {
       controlPanelMotor.set(ControlMode.PercentOutput, 0);
+    }
+  }
+
+  public static void positionControl(char initColor) {
+    String gameData = DriverStation.getInstance().getGameSpecificMessage();
+    List<Character> colors = new ArrayList<Character>();
+    colors.add('B');
+    colors.add('G');
+    colors.add('R');
+    colors.add('Y');
+    if(gameData.length() > 0) {
+      int placement = colors.indexOf(gameData.charAt(0)) - colors.indexOf(initColor); // Distance between colors
+      if(placement < 0) { 
+        if(placement == 3) { // If the color is 3 rotations away one direction, make it turn 1 rotation the opposite direction
+          colorRotation(1, 1);
+        } else {
+          colorRotation(Math.abs(placement), -1);
+        }
+      } else if(placement > 0) {
+        if(placement == 3) {
+          colorRotation(1, -1);
+        } else {
+          colorRotation(Math.abs(placement), 1);
+        }
+      }
+    } else {
+      //Code for no data received yet
     }
   }
 
