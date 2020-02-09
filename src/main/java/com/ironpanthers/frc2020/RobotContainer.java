5f37bd1079444d1fbe9d3ef3f4066913649ec674
Chloe <chloeyan365@gmail.com>
2020-02-08 16:31:06 -0800

    Delete unnecessary files

--- src/main/java/com/ironpanthers/frc2020/RobotContainer.java
+++ src/main/java/com/ironpanthers/frc2020/RobotContainer.java
@@ -9,9 +9,7 @@ package com.ironpanthers.frc2020;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
-import com.ironpanthers.frc2020.commands.ExampleCommand;
 import com.ironpanthers.frc2020.subsystems.ColorMatcher;
-import com.ironpanthers.frc2020.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 
 /**
@@ -23,10 +21,7 @@ import edu.wpi.first.wpilibj2.command.Command;
  */
 public class RobotContainer {
 	// The robot's subsystems and commands are defined here...
-	private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 	private final ColorMatcher colorMatcher = new ColorMatcher();
-
-	private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
 
 	/**
 	 * The container for the robot. Contains subsystems, OI devices, and commands.
@@ -50,8 +45,8 @@ public class RobotContainer {
 	 *
 	 * @return the command to run in autonomous
 	 */
-	public Command getAutonomousCommand() {
+	/*public Command getAutonomousCommand() {
 		// An ExampleCommand will run in autonomous
 		return m_autoCommand;
-	}
+	}*/
 }
