e8a7c7a8e24e9f47ecf9f5c5a2f36c6d4aac828d
James Varah <james@varah.com>
2020-02-15 17:06:24 -0800

    Change Shoot Preloaded to Parallel

--- src/main/java/com/ironpanthers/frc2020/auto/commands/ShootPreLoaded.java
+++ src/main/java/com/ironpanthers/frc2020/auto/commands/ShootPreLoaded.java
@@ -7,25 +7,25 @@
 
 package com.ironpanthers.frc2020.auto.commands;
 
-import com.ironpanthers.frc2020.Constants.Conveyor;
 import com.ironpanthers.frc2020.commands.arm.ArmAndSpinShooter;
+import com.ironpanthers.frc2020.commands.arm.ArmToTarget;
 import com.ironpanthers.frc2020.commands.shooter.ShooterSequence;
 import com.ironpanthers.frc2020.subsystems.Arm;
 import com.ironpanthers.frc2020.subsystems.ConveyorBelt;
 import com.ironpanthers.frc2020.subsystems.Shooter;
 
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/latest/docs/software/commandbased/convenience-features.html
-public class ShootPreLoaded extends SequentialCommandGroup {
+public class ShootPreLoaded extends ParallelCommandGroup {
 	/**
 	 * Creates a new ShootPreLoaded.
 	 */
 	public ShootPreLoaded(Arm arm, int target, Shooter shooter, int velocity, int threshold, ConveyorBelt conveyor) {
 		// Add your commands in the super() call, e.g.
 		// super(new FooCommand(), new BarCommand());
-		super(new ArmAndSpinShooter(arm, target, shooter, velocity, threshold), new ShooterSequence(shooter, conveyor, velocity, threshold));
+		super(new ArmToTarget(arm, target), new ShooterSequence(shooter, conveyor, velocity, threshold));
 	}
 }
