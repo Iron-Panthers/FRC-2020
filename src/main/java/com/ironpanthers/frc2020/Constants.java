bfdf4a35d2559cfabed5954bdfff9ddc79ebb585
Ilan Rosenbaum <ilan@al.net>
2020-02-27 15:46:04 -0800

    Constants Changes For Conveyor

--- src/main/java/com/ironpanthers/frc2020/Constants.java
+++ src/main/java/com/ironpanthers/frc2020/Constants.java
@@ -112,7 +112,7 @@ public final class Constants {
 
         // Encoder Stuff
         public static final int kPositionErrorTolerance = 350;
-        public static final int kShiftEncoderDistance = 18500;//was 16000 
+        public static final int kShiftEncoderDistance = 16000;
 
     }
 
