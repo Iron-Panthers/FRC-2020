991365c4f80a127dc45b3b3cea54f006d9e0f72f
5026-DS-1 <5026craptop@gmail.com>
2020-03-01 13:03:48 -0800

    Reduce Drive Current
    Hypothesis is that the motors are tripping the breaker, leading to dropped can frames

--- src/main/java/com/ironpanthers/frc2020/Constants.java
+++ src/main/java/com/ironpanthers/frc2020/Constants.java
@@ -61,7 +61,7 @@ public final class Constants {
 
         public static final double kP = 2.6;
 
-        public static final double kCurrentLimit = 60; // amps
+        public static final double kCurrentLimit = 40.0; // amps
         public static final double kRampRate = 0.5; // seconds to full power
     }
 
