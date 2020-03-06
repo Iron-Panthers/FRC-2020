3336f808560cdb16dd01cb8780f7ad88c17c9f4b
James Varah <james@varah.com>
2020-03-05 20:18:00 -0800

    Update Camera Rotation

--- driverstation/start-client-5804.bat
+++ driverstation/start-client-5804.bat
@@ -6,6 +6,6 @@
 
 :: For convenience, creating a shortcut on the desktop to this script is recommended
 
-gst-launch-1.0 -e udpsrc port=5804 ! "application/x-rtp, clock-rate=(int)90000, media=(string)video, encoding-name=VP8" ! rtpvp8depay ! tee name = t t. ! queue ! decodebin ! videoflip method=clockwise ! videoflip method=clockwise ! videoflip method=clockwise ! autovideosink t. ! queue ! matroskamux ! filesink location=%time:~0,2%_%time:~3,2%_%time:~6,2%__%date:~4,2%_%date:~7,2%_%date:~10,4%_5804.mkv
+gst-launch-1.0 -e udpsrc port=5804 ! "application/x-rtp, clock-rate=(int)90000, media=(string)video, encoding-name=VP8" ! rtpvp8depay ! tee name = t t. ! queue ! decodebin ! videoflip method=clockwise ! videoflip method=clockwise ! autovideosink t. ! queue ! matroskamux ! filesink location=%time:~0,2%_%time:~3,2%_%time:~6,2%__%date:~4,2%_%date:~7,2%_%date:~10,4%_5804.mkv
 pause
 :: If the script fails then wait for confirmation before closing
