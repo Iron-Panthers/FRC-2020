11372f08b9d4866cc5390919fd94d43278737af5
5026-DS-1 <5026craptop@gmail.com>
2021-04-04 16:53:08 -0700

    Reduce conveyor spacing distance

--- src/main/java/com/ironpanthers/frc2020/Constants.java
+++ src/main/java/com/ironpanthers/frc2020/Constants.java
@@ -116,7 +116,7 @@ public final class Constants {
 
         // Encoder Stuff
         public static final int kPositionErrorTolerance = 350;
-        public static final int kShiftEncoderDistance = 26000 * 5/3;
+        public static final int kShiftEncoderDistance = 26000 * 4/3;
 		public static final int kShiftEncoderDistanceLast = 11000;
     }
 
